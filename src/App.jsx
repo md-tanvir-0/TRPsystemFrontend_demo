078a01a39bde64a72407ee191ad502ff3c93b118
Arif Absar <arifabsar182@gmail.com>
2024-05-29 16:42:33 +0600

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
 
-
 const navigation = [
   {
     name: 'Analyze',
@@ -28,7 +27,7 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function App() {
+export default function Sidebar(){
   const [sidebarOpen, setSidebarOpen] = useState(false);
   const [dropdowns, setDropdowns] = useState({});
 
@@ -89,48 +88,30 @@ export default function App() {
           </div>
         </div>
       </div>
+      </div>
+      );
+}
+export default function App(){
 
-      <div className="flex flex-col w-0 flex-1 overflow-hidden">
-        <div className="md:hidden pl-1 pt-1 sm:pl-3 sm:pt-3">
-          <button
-            className="-ml-0.5 -mt-0.5 h-12 w-12 inline-flex items-center justify-center rounded-md text-gray-500 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500"
-            onClick={() => setSidebarOpen(true)}
-          >
-            <span className="sr-only">Open sidebar</span>
-            <svg
-              className="h-6 w-6"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              aria-hidden="true"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M4 6h16M4 12h16M4 18h16"
-              />
-            </svg>
-          </button>
+  return (
+    <><div> <Sidebar /></div><main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
+      <div className="py-6">
+        <div><Sidebar/></div>
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          <h1 className="text-2xl font-bold text-gray-900">TRP SYSTEM</h1>
         </div>
-        <main className="flex-1 relative z-0 overflow-y-auto focus:outline-none">
-          <div className="py-6">
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              <h1 className="text-2xl font-bold text-gray-900">TRP SYSTEM</h1>
-            </div>
-            <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
-              {/* Replace with your content */}
-              <div className="py-4">
-                <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
-                  {/* Content goes here */}
-                </div>
-              </div>
-              {/* /End replace */}
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 md:px-8">
+          {/* Replace with your content */}
+          <div className="py-4">
+            <div className="border-4 border-dashed border-gray-200 rounded-lg h-96">
+              {/* Content goes here */}
             </div>
           </div>
-        </main>
+          {/* /End replace */}
+        </div>
       </div>
-    </div>
+    </main></>
+      
+    
   );
 }
